cc4dbe8905c1e54c98dbdd8c76e4c99d60d49a1d
Chris Mims <chritopherm970@aol.com>
2020-09-07 14:42:57 -0500

    Added model file.

---
+++ models/workout.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    required: true,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: true,
+      },
+      name: { type: String, required: true },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      weight: Number,
+      reps: Number,
+      sets: Number,
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
