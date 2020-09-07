cfa0ccd283e9174595fc78b894f588c374ad0987
Chris Mims <chritopherm970@aol.com>
2020-09-07 15:08:01 -0500

    Added post route.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,4 +7,14 @@ router.get("/api/workouts",(req, res) => {
         res.json(dbWorkout);
     });
 });
+
+router.post("/api/workouts", (req, res)=> {
+    db.Workout.create(req.body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
 module.exports = router;
