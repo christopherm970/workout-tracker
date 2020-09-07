ec40caab8a8733a1d83389d57e48155593614f51
Chris Mims <chritopherm970@aol.com>
2020-09-07 14:56:11 -0500

    Added get route.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,10 @@
 const router = require("express").Router();
+const db = require("../models");
 
-
+router.get("/api/workouts",(req, res) => {
+    db.Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    });
+});
 module.exports = router;
